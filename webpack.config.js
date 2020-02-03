19ababf6aed80d0a4a630fbf8d4048c0974adaa5
JRileyH <riley@sleepy.fish>
2020-02-03 08:26:30 -0500

    got npm deployment cleaned up

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 const path = require('path');
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const env = process.env.NODE_ENV;
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bogie: './src/index.js',
+    'bogie.min': './src/index.js'
+  },
   output: {
-    filename: 'bogie.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     library: 'Bogie',
     libraryTarget: 'window',
     libraryExport: 'default'
   },
-  devtool: env === 'development' ? 'inline-source-map' : false,
+  devtool: env === 'development' ? 'inline-source-map' : 'source-map',
   devServer: {
     contentBase: [
       path.resolve(__dirname, 'examples'),
@@ -23,8 +27,19 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    env === 'development' ? new webpack.HotModuleReplacementPlugin() : () => {}
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        include: /\.min\.js$/
+      })
+    ]
+  },
+  performance: {
+    hints: false
+  },
   module: {
     rules: [
       {
