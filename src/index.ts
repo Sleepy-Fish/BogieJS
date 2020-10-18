432776ed7b27aa04622caa1e3fed796142cc62fb
riggy <riley@sleepy.fish>
2020-10-17 22:38:43 -0400

    export all members

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import Physics from './physics';
 import Geom from './geom';
 export { default as Physics } from './physics';
+export { default as World } from './physics/World';
+export { default as Watcher } from './physics/Watcher';
 export { default as Geom } from './geom';
+export { default as Point } from './geom/Point';
+export { default as Vector } from './geom/Vector';
+export { default as Line } from './geom/Line';
+export { default as Rectangle } from './geom/Rectangle';
+export { default as Circle } from './geom/Circle';
 export default {
   Physics,
   Geom,
